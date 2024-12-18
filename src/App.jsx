43145dbd89e72d4017a3fc2a57c5e6cc5c001aa4
kourjot <pkour6188@gmail.com>
2024-12-18 10:19:30 +0530

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+// import React from 'react'
 
-const main = () => {
-  return (
-    <div>
+// const main = () => {
+//   return (
+//     <div>
       
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
-export default main
+// export default main
